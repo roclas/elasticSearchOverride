68cb105401ca012352851e397856b71c2b65bc5d
carlos <carlopolo@gmail.com>
2017-12-01 04:49:08 +0000

    bug in liferay-type-mappings.json that broke the app after reindexing everything from the control panel

--- build_and_deploy_into_server.sh
+++ build_and_deploy_into_server.sh
@@ -6,9 +6,6 @@ gradle build
 file=$(find . -name "*.jar" | tail -1 )
 LIFERAY_HOME=/home/carlos/Liferay/bundles/dxp/liferay-dxp-digital-enterprise-7.0-sp6
 
-#mkdir -p $LIFERAY_HOME/osgi/marketplace/override/
-#cp ./build/libs/com.liferay.portal.search.elasticsearch-2.1.15.jar $LIFERAY_HOME/osgi/marketplace/override/
-
+./clean_server.sh
 cp $file $LIFERAY_HOME/deploy/
-rm -rf $LIFERAY_HOME/osgi/state
 
